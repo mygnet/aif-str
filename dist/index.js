b667858a2d4645d78734edd557264e4314ad2f0a
martin <martin@anemona.com>
2021-06-03 19:21:09 -0500

    added documentation to methods

--- dist/index.js
+++ dist/index.js
@@ -1,44 +1,27 @@
-'use strict';
-
-exports.__esModule = true;
-exports.split = split;
-exports.ucfirst = ucfirst;
-exports.camelize = camelize;
-exports.pad = pad;
-exports.padRight = padRight;
-/**
+'use strict';exports.__esModule=!0,exports.split=split,exports.ucfirst=ucfirst,exports.camelize=camelize,exports.pad=pad,exports.padLeft=padLeft;/**
  * Divide a chain into pieces according to a size
- * @param {string} data
- * @param {integer} splitLength
- */
-function split(data, splitLength) {
-  splitLength = splitLength === undefined ? 1 : parseInt(splitLength);
-  if (data === undefined || splitLength < 1) return false;
-  data += '';
-  var chunks = [];
-  var pos = 0;
-  var len = data.length;
-  while (pos < len) {
-    chunks.push(data.slice(pos, pos += splitLength));
-  }return chunks;
-}
-
-function ucfirst(str) {
-  return str.charAt(0).toUpperCase() + str.slice(1);
-}
-
-function camelize(str, ex) {
-  ex || (ex = /\/(.)/);
-  return ucfirst(str.replace(RegExp(ex, 'g'), function ($1) {
-    return $1.toUpperCase();
-  }));
-}
-
-function pad(str, len, c, ty) {
-  var pad = Array(len).fill(c || ' ').join('');
-  return (ty ? '' : str) + pad.substring(0, pad.length - str.length) + (ty ? str : '');
-}
-
-function padRight(str, len, c) {
-  return pad(str, len, c, true);
-}
+ * @param {string} data source string
+ * @param {integer} splitLength number of chunks into which a chain will be divided
+ */function split(a,b){if(b=void 0===b?1:parseInt(b),void 0===a||1>b)return!1;a+='';for(var c=[],d=0,e=a.length;d<e;)c.push(a.slice(d,d+=b));return c}/**
+ * Transforms the first letter in uppercase of a given string
+ * @param {tring} str source string
+ * @returns 
+ */function ucfirst(a){return a.charAt(0).toUpperCase()+a.slice(1)}/**
+ * Transform to camelCase a string given an expression
+ * @param {string} str source string
+ * @param {string} ex expression used for string separation
+ * @returns 
+ */function camelize(a,b){return b||(b=/\/(.)/),ucfirst(a.replace(RegExp(b,'g'),function(a){return a.toUpperCase()}))}/**
+ * Fill a string to the left or right
+ * @param {string} str source string
+ * @param {integer} len character size to fill
+ * @param {char} c padding character, defaults to whitespace
+ * @param {boolean} ty if true it fills to the left, otherwise it will fill to the right
+ * @returns 
+ */function pad(a,b,d,c){var e=Array(b).fill(d||' ').join('');return(c?'':a)+e.substring(0,e.length-a.length)+(c?a:'')}/**
+ * Fill a string to the left
+ * @param {string} str source string
+ * @param {integer} len character size to fill
+ * @param {char} c padding character, defaults to whitespace
+ * @returns 
+ */function padLeft(a,b,d){return pad(a,b,d,!0)}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 /**
  * Divide a chain into pieces according to a size
- * @param {string} data
- * @param {integer} splitLength
+ * @param {string} data source string
+ * @param {integer} splitLength number of chunks into which a chain will be divided
  */
 export function split(data, splitLength) {
   splitLength = splitLength === undefined ? 1 : parseInt(splitLength)
@@ -13,23 +13,45 @@ export function split(data, splitLength) {
   while (pos < len) chunks.push(data.slice(pos, pos += splitLength))
   return chunks
 }
-
+/**
+ * Transforms the first letter in uppercase of a given string
+ * @param {tring} str source string
+ * @returns 
+ */
 export function ucfirst(str) {
   return str.charAt(0).toUpperCase() + str.slice(1)
 }
-
+/**
+ * Transform to camelCase a string given an expression
+ * @param {string} str source string
+ * @param {string} ex expression used for string separation
+ * @returns 
+ */
 export function camelize(str, ex) {
   ex || (ex = /\/(.)/)
   return ucfirst(str.replace(RegExp(ex, 'g'), function($1) {
     return $1.toUpperCase()
   }))
 }
-
+/**
+ * Fill a string to the left or right
+ * @param {string} str source string
+ * @param {integer} len character size to fill
+ * @param {char} c padding character, defaults to whitespace
+ * @param {boolean} ty if true it fills to the left, otherwise it will fill to the right
+ * @returns 
+ */
 export function pad(str, len, c, ty) {
   const pad = Array(len).fill(c || ' ').join('')
   return (ty ? '' : str) + pad.substring(0, pad.length - str.length) + (ty ? str : '')
 }
-
-export function padRight(str, len, c) {
+/**
+ * Fill a string to the left
+ * @param {string} str source string
+ * @param {integer} len character size to fill
+ * @param {char} c padding character, defaults to whitespace
+ * @returns 
+ */
+export function padLeft(str, len, c) {
   return pad(str, len, c, true)
 }
